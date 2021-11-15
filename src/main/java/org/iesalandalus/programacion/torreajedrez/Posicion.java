ce105879b6c62b5e14fc8a7e1e5f06d86f63d8ec
mcrui <mcruizgarcia@gmail.com>
2021-11-15 20:16:03 +0100

    4.
    Crea la clase Posicion. Crea los atributos fila (int) y columna (char) con la visibilidad adecuada.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -4,3 +4,4 @@ public class Posicion {
     private int fila;
     private char columna;
 }
+//El commit correspondiente es : Crea la clase Posicion. Crea los atributos fila (int) y columna (char) con la visibilidad adecuada.
