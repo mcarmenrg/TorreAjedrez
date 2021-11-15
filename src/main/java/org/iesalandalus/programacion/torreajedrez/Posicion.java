9b1c4cc1944b9fce74f2fe838512ed24c307e2e6
mcrui <mcruizgarcia@gmail.com>
2021-11-15 20:38:00 +0100

    5.
    Crea los métodos get y set para los atributos. Recuerda que para el método set se debe tener en cuenta que los valores indicados sean correctos (las filas van del 1 al 8 -ambos inclusive- y las columnas de la 'a' a la 'h' -ambas inclusive-) y si no se lance una excepción del tipo IllegalArgumentException con el mensaje adecuado.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -3,5 +3,34 @@ package org.iesalandalus.programacion.torreajedrez;
 public class Posicion {
     private int fila;
     private char columna;
+
+    public int getFila() {
+        return fila;
+    }
+
+    private void setFila(int fila) {
+
+        if (fila>=1 && fila<=8){
+            this.fila = fila;
+        } else {
+            throw new IllegalArgumentException("ERROR: Fila no válida.");
+        }
+
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        if (columna>='a' && columna<='h'){
+            this.columna = columna;
+        }else {
+            throw new IllegalArgumentException("ERROR: Columna no válida.");
+
+        }
+
+    }
 }
-//El commit correspondiente es : Crea la clase Posicion. Crea los atributos fila (int) y columna (char) con la visibilidad adecuada.
+
+
