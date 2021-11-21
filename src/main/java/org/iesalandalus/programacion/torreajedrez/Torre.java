969ec4d1481e50c4c0e7317ef9612a2773345e4c
mcrui <mcruizgarcia@gmail.com>
2021-11-21 20:47:50 +0100

    11. Crea los métodos get y set para cada atributo con la visibilidad adecuada. Estos métodos siempre comprobarán la validez de los parámetros pasados y si no son correctos deberá lanzar la excepción adecuada (NullPointerException o IllegalArgumentException) con el mensaje adecuado

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -5,8 +5,27 @@ public class Torre
     private Color color;
     private Posicion posicion;
 
+    public Color getColor() {
+        return color;
+    }
+
+    private void setColor(Color color) {
+        if (color != null)
+        {
+            this.color = color;
+        }else {
+            throw new NullPointerException ("ERROR: No se puede asignar un color nulo.");
+        }
 
 
 
+    }
 
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    private void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
 }
