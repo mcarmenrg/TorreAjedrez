6fed2c831d9449a9a94b808f81d48c9f871e1d1f
mcrui <mcruizgarcia@gmail.com>
2021-11-28 21:39:14 +0100

    20.void mostrarTorre(): mostrará por consola la representación de la torre representado por el atributo de clase torre (crea el atributo si aún no lo has hecho).

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -1,7 +1,15 @@
 package org.iesalandalus.programacion.torreajedrez;
 
 public class MainApp {
+	private static Torre torre = null;
+	private static void mostrarTorre(){
+		if (torre == null){
+			System.out.print("No se ha creado torre aún");
+		}else {
+			System.out.print("Torre es "+ torre );
+		}
 
+	}
 	public static void main(String[] args) {
 		System.out.println("kk");
 	}
